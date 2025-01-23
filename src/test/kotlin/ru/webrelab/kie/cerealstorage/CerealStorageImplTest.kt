e16f6f3b9cf133c84331b339be6e228acec83fb7
AFerster <ferster_a@flowwow.com>
2025-01-23 14:23:10 +0300

    fixes

--- src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
+++ src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
@@ -2,32 +2,27 @@ package ru.webrelab.kie.cerealstorage
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import kotlin.random.Random
 
 class CerealStorageImplTest {
 
-    private val storage = CerealStorageImpl(10f, 20f)
+    private val containerCapacity = 10f
 
-    private fun randomCereal(): Cereal = Cereal.entries.toTypedArray().random()
+    private val storageCapacity = 20f
 
-    private fun randomFloat(min: Float = 0f, max: Float = 10f): Float {
-        require(min <= max) { "Min must not be greater than max" }
-        return Random.nextFloat() * (max - min) + min
-    }
+    private val storage = CerealStorageImpl(containerCapacity, storageCapacity)
+
 
     @Test
     fun `should throw if containerCapacity is negative`() {
         assertThrows(IllegalArgumentException::class.java) {
-            CerealStorageImpl(randomFloat(-10f, -1f), 10f)
+            CerealStorageImpl(-1.1f, 10f)
         }
     }
 
     @Test
     fun `should throw if storageCapacity is lower than containerCapacity`() {
-        val containerCapacity = randomFloat(5f, 10f)
-        val storageCapacity = randomFloat(1f, 4f)
         assertThrows(IllegalArgumentException::class.java) {
-            CerealStorageImpl(containerCapacity, storageCapacity)
+            CerealStorageImpl(6.6f, 3.3f)
         }
     }
 
@@ -35,98 +30,131 @@ class CerealStorageImplTest {
 
     @Test
     fun `addCereal should throw if added amount of cereal is lower than 0`() {
-        val randomCereal = randomCereal()
         assertThrows(IllegalArgumentException::class.java) {
-            storage.addCereal(randomCereal, randomFloat(-10f, -1f))
+            storage.addCereal(Cereal.RICE, -1.1f)
         }
     }
 
     @Test
     fun `addCereal should throw if no more containers can be placed in storage`() {
-        storage.addCereal(randomCereal(), 10f)
-        storage.addCereal(randomCereal(), 10f)
+        storage.addCereal(Cereal.BULGUR, 10f)
+        storage.addCereal(Cereal.RICE, 10f)
 
         assertThrows(IllegalStateException::class.java) {
-            storage.addCereal(randomCereal(), 10f)
+            storage.addCereal(Cereal.PEAS, 10f)
+        }
+    }
+
+    @Test
+    fun `addCereal should throw if no more containers can be placed in storage (added container is hollow)`() {
+        storage.addCereal(Cereal.BULGUR, 10f)
+        storage.addCereal(Cereal.RICE, 10f)
+
+        assertThrows(IllegalStateException::class.java) {
+            storage.addCereal(Cereal.PEAS, 0f)
         }
     }
 
     @Test
     fun `addCereal return 0f as excessive amount if container is added and containerCapacity is not exceeded`() {
-        val randomCereal = randomCereal()
-        val amount = randomFloat(1f, 9f)
-        val excessiveAmount = storage.addCereal(randomCereal, amount)
+
+        val excessiveAmount = storage.addCereal(Cereal.RICE, 1.1f)
+
+        assertEquals(0f, excessiveAmount)
+    }
+
+    @Test
+    fun `addCereal return 0f as excessive amount if container is added, added amount is 0f`() {
+
+        val excessiveAmount = storage.addCereal(Cereal.RICE, 0f)
 
         assertEquals(0f, excessiveAmount)
     }
 
     @Test
     fun `addCereal returns correct excessive amount if container is added and capacity is lower than added amount`() {
-        val randomCereal = randomCereal()
-        val amount = randomFloat(11f, 20f)
-        val excessiveAmount = storage.addCereal(randomCereal, amount)
+        val amount = 12.2f
 
-        assertEquals(amount - 10f, excessiveAmount)
+        val excessiveAmount = storage.addCereal(Cereal.RICE, amount)
+
+        assertEquals(amount - containerCapacity, excessiveAmount)
     }
 
     @Test
     fun `addCereal returns 0f if container exist and capacity is not exceeded`() {
-        val randomCereal = randomCereal()
-        storage.addCereal(randomCereal, 8f)
+        storage.addCereal(Cereal.RICE, 1.1f)
 
-        val excessiveAmount = storage.addCereal(randomCereal, 2f)
+        val excessiveAmount = storage.addCereal(Cereal.RICE, 2f)
+
+        assertEquals(0f, excessiveAmount)
+    }
+
+    @Test
+    fun `addCereal returns 0f if container exist and amount is 0f`() {
+        storage.addCereal(Cereal.RICE, 1.1f)
+
+        val excessiveAmount = storage.addCereal(Cereal.RICE, 0f)
 
         assertEquals(0f, excessiveAmount)
     }
 
     @Test
     fun `addCereal returns correct excessive amount if container exist and capacity is exceeded`() {
-        val randomCereal = randomCereal()
-        val storageAmount = randomFloat(1f, 10f)
 
-        storage.addCereal(randomCereal, storageAmount)
+        val initialAmount = 3.3f
+        val addedAmount = 8.8f
 
-        val addedAmount = randomFloat(10f, 20f)
+        storage.addCereal(Cereal.RICE, initialAmount)
 
-        val excessiveAmount = storage.addCereal(randomCereal, addedAmount)
+        val excessiveAmount = storage.addCereal(Cereal.RICE, addedAmount)
 
-        val expectedValue = storageAmount + addedAmount - 10f
+        val expectedValue =  initialAmount + addedAmount - containerCapacity
 
         assertEquals(expectedValue, excessiveAmount)
     }
     // Test for getCereal
     @Test
     fun `getCereal should throw if added amount of cereal is lower than 0`() {
-        val randomCereal = randomCereal()
         assertThrows(IllegalArgumentException::class.java) {
-            storage.getCereal(randomCereal, randomFloat(-10f, -1f))
+            storage.getCereal(Cereal.RICE, -1.1f)
         }
     }
 
     @Test
     fun `getCereal returns correct amount if taken amount is lower than stored`() {
-        val randomCereal = randomCereal()
-        val storageAmount = randomFloat(1f, 10f)
+        val storageAmount = 8f
 
-        storage.addCereal(randomCereal, storageAmount)
+        storage.addCereal(Cereal.RICE, storageAmount)
 
-        val amount = randomFloat(1f, storageAmount)
+        val amount = 4.4f
 
-        val takenAmount = storage.getCereal(randomCereal, amount)
+        val takenAmount = storage.getCereal(Cereal.RICE, amount)
+
+        assertEquals(amount, takenAmount)
+    }
+
+    @Test
+    fun `getCereal returns correct amount if taken amount is 0f`() {
+        val storageAmount = 8f
+
+        storage.addCereal(Cereal.RICE, storageAmount)
+
+        val amount = 0f
+
+        val takenAmount = storage.getCereal(Cereal.RICE, amount)
 
         assertEquals(amount, takenAmount)
     }
 
     @Test
     fun `getCereal returns correct amount if taken amount is higher than stored`() {
-        val randomCereal = randomCereal()
-        val storageAmount = randomFloat(1f, 10f)
+        val storageAmount = 8f
 
-        storage.addCereal(randomCereal, storageAmount)
+        storage.addCereal(Cereal.RICE, storageAmount)
 
-        val amount = randomFloat(storageAmount, 10f)
+        val amount = 9.9f
 
-        val takenAmount = storage.getCereal(randomCereal, amount)
+        val takenAmount = storage.getCereal(Cereal.RICE, amount)
 
         assertEquals(storageAmount, takenAmount)
     }
@@ -134,7 +162,7 @@ class CerealStorageImplTest {
     @Test
     fun `getCereal should throw exception if cereal container is not in storage`() {
         assertThrows(IllegalStateException::class.java) {
-            storage.getCereal(randomCereal(), randomFloat(1f, 10f))
+            storage.getCereal(Cereal.RICE, 1f)
         }
     }
 
@@ -142,20 +170,19 @@ class CerealStorageImplTest {
 
     @Test
     fun `removeContainer returns true if it  removes hollow container`() {
-        val randomCereal = randomCereal()
-        storage.addCereal(randomCereal, 0f)
+        storage.addCereal(Cereal.RICE, 0f)
 
-        val result = storage.removeContainer(randomCereal)
+        val result = storage.removeContainer(Cereal.RICE)
 
         assertTrue(result)
     }
 
     @Test
     fun `removeContainer returns false if container is not removed`() {
-        val randomCereal = randomCereal()
-        storage.addCereal(randomCereal, 1f)
+        storage.addCereal(Cereal.RICE, 1f)
 
-        val result = storage.removeContainer(randomCereal)
+
+        val result = storage.removeContainer(Cereal.RICE)
 
         assertFalse(result)
     }
@@ -163,7 +190,7 @@ class CerealStorageImplTest {
     @Test
     fun `removeContainer should throw exception if cereal container is not in storage`() {
         assertThrows(IllegalStateException::class.java) {
-            storage.removeContainer(randomCereal())
+            storage.removeContainer(Cereal.RICE)
         }
     }
 
@@ -172,11 +199,10 @@ class CerealStorageImplTest {
 
     @Test
     fun `getAmount should return the amount of cereal in storage`() {
-        val randomCereal = randomCereal()
-        val amount = randomFloat(1f, 10f)
-        storage.addCereal(randomCereal, amount)
+        val amount = 5.5f
+        storage.addCereal(Cereal.PEAS, amount)
 
-        val resultAmount = storage.getAmount(randomCereal)
+        val resultAmount = storage.getAmount(Cereal.PEAS)
 
         assertEquals(amount, resultAmount)
     }
@@ -184,7 +210,7 @@ class CerealStorageImplTest {
     @Test
     fun `getAmount should throw exception if cereal container is not in storage`() {
         assertThrows(IllegalStateException::class.java) {
-            storage.getAmount(randomCereal())
+            storage.getAmount(Cereal.PEAS)
         }
     }
 
@@ -192,19 +218,18 @@ class CerealStorageImplTest {
 
     @Test
     fun `getSpace should return remaining space in container`() {
-        val randomCereal = randomCereal()
-        val amount = randomFloat(1f, 10f)
-        storage.addCereal(randomCereal, amount)
+        val amount = 5.5f
+        storage.addCereal(Cereal.MILLET, amount)
 
-        val space = storage.getSpace(randomCereal)
+        val space = storage.getSpace(Cereal.MILLET)
 
-        assertEquals(10f - amount, space)
+        assertEquals(containerCapacity - amount, space)
     }
 
     @Test
     fun `getSpace should throw exception if cereal container is not in storage`() {
         assertThrows(IllegalStateException::class.java) {
-            storage.getSpace(randomCereal())
+            storage.getSpace(Cereal.MILLET)
         }
 
     }
@@ -214,8 +239,8 @@ class CerealStorageImplTest {
         val cereal1 = Cereal.PEAS
         val cereal2 = Cereal.MILLET
 
-        val amount1 = randomFloat(1f, 10f)
-        val amount2 = randomFloat(1f, 10f)
+        val amount1 = 5.4f
+        val amount2 = 8.3f
 
         storage.addCereal(cereal1, amount1)
         storage.addCereal(cereal2, amount2)
